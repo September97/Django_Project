87829ec769b2c25e8a1742946f51126c14d5664b
Noluthando September <NSeptember591@gmail.com>
2025-11-10 21:32:04 +0200

    added functinality

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,12 +1,15 @@
 # Create your models here.
 from django.db import models
+from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    date_posted = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
-
-
+    
+    class Meta:
+        ordering = ['-created_at']
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def add_post(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        author = request.POST.get('author')
+        
+        Post.objects.create(
+            title=title,
+            content=content,
+            author=author
+        )
+        return redirect('post_list')
+    
+    return render(request, 'blog/add_post.html')
